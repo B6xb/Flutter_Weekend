32dbba650a6702d15f618ef1120ebdf7283b5b79
Bander Almutairi <bosking@Banders-MacBook-Pro.local>
2022-11-18 21:25:19 +0300

    SignUp Updated

---
+++ lib/screens/khawiSignUp.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:khawi/constants.dart';
+import 'package:khawi/screens/khawiSignIn.dart';
+
+class KhawiSignUp extends StatelessWidget {
+  const KhawiSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome',
+                  style: kMainTextStyle,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Your Khawi is always there',
+                  style: kMainTextStyle.copyWith(
+                    letterSpacing: 0.5,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 15,
+                    color: kButtonsColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: kColor,
+                    ),
+                    child: TextField(
+                      cursorColor: kMainColor,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        prefixIcon: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Icon(null),
+                        ),
+                        fillColor: kColor,
+                        hintText: 'First Name',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: kColor,
+                    ),
+                    child: TextField(
+                      cursorColor: kMainColor,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(
+                            borderSide: BorderSide.none),
+                        prefixIcon: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Icon(null),
+                        ),
+                        fillColor: kColor,
+                        hintText: 'Last Name',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: kColor,
+              ),
+              child: TextField(
+                cursorColor: kMainColor,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(borderSide: BorderSide.none),
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Icon(Icons.email_outlined),
+                  ),
+                  fillColor: kColor,
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: kColor,
+              ),
+              child: TextField(
+                cursorColor: kMainColor,
+                obscureText: true,
+                decoration: InputDecoration(
+                  focusColor: Colors.green,
+                  border: const OutlineInputBorder(borderSide: BorderSide.none),
+                  prefixIconColor: kButtonsColor,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Icon(Icons.password),
+                  ),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: kColor,
+              ),
+              child: TextField(
+                cursorColor: kMainColor,
+                obscureText: true,
+                decoration: InputDecoration(
+                  focusColor: Colors.green,
+                  border: const OutlineInputBorder(borderSide: BorderSide.none),
+                  prefixIconColor: kButtonsColor,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Icon(Icons.password),
+                  ),
+                  suffixIcon: GestureDetector(
+                    child: const Icon(Icons.arrow_forward_rounded),
+                    onTap: () {},
+                  ),
+                  hintText: 'Confirm Password',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: kMainColor,
+    );
+  }
+}
