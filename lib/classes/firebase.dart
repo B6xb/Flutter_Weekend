ce98fba272f0b9cf2993a4c8dc2495d6e5f9e611
max0man0 <0naxman0@gmail.com>
2022-11-18 21:46:18 +0300

    commmit

--- lib/classes/firebase.dart
+++ lib/classes/firebase.dart
@@ -1,13 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
-class Firebase{
-
-FirebaseAuth auth = FirebaseAuth.instance;
-
-
-
-
-
-
+class Firebase {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final firestore = FirebaseFirestore.instance;
 }
