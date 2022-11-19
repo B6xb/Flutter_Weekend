103849d2ada10bea0fddb5693daf37d2b422dbc9
PUT_YOUR_NAME_HERE <MY_NAME@example.com>
2022-11-19 14:25:36 +0300

    fix packeg

--- lib/getPage.dart
+++ lib/getPage.dart
@@ -3,7 +3,9 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:khawi/screens/khawiMainPage.dart';
+import 'package:khawi/screens/khawiPackages.dart';
 import 'package:khawi/screens/khawiSignIn.dart';
+import 'package:khawi/screens/packagePage.dart';
 import 'package:khawi/util/firebase_service.dart';
 
 class GetPage {
@@ -16,7 +18,7 @@ class GetPage {
     if (GetPage.authChange == null) {
       return KhawiSignIn();
     } else {
-      return KhawiHomePage();
+      return packagePage();
     }
   }
 }
